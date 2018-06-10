ce23e4e80b2d4551d77e55441246bb6d022e6f12
Bentley, Matt (UK - London) <matthew_j_bentley@hotmail.com>
2018-06-10 17:58:24 +0100

    Initial

---
+++ RabbitMQDemo.Worker/Program.cs
@@ -0,0 +1,66 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQDemo.Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guid workerId = Guid.NewGuid();
+            Console.WriteLine($"Starting Worker {workerId}");
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "192.168.99.101",
+                UserName = "test_user",
+                Password = "test_user",
+                VirtualHost = "test",
+                AutomaticRecoveryEnabled = true, // automatically try to recover connection if fails (default 5s wait)
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "task_queue",
+                                 durable: true, // persist queue on server failure
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    // send single item to workers only when they are free
+                    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                    Console.WriteLine($" [{workerId}] Waiting for messages.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine($" [{workerId}] Received {0}", message);
+
+                        int dots = message.Split('!').Length - 1;
+                        Thread.Sleep(dots * 1000);
+
+                        Console.WriteLine($" [{workerId}] finished processing {message}");
+
+                        // send acknowledgement to server to notify finished processing
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    };
+                    // manually send acknowledgements back to the server
+                    channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine($"Stopped Worker {workerId}.");
+        }
+    }
+}
---
+++ RabbitMQDemo/Program.cs
@@ -0,0 +1,100 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQDemo.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Client...");
+
+            //SendSimpleMessage();
+            StartQueue();
+
+            Console.WriteLine("Finished Client. Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static void SendSimpleMessage()
+        {
+            var factory = new ConnectionFactory() { HostName = "192.168.99.101" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+        }
+
+        private static void StartQueue()
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "192.168.99.101",
+                UserName = "test_user",
+                Password = "test_user",
+                VirtualHost = "test"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "task_queue",
+                                 durable: true, // persist queue on server failure
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    var properties = channel.CreateBasicProperties();
+                    // persist messages on server failure
+                    properties.Persistent = true;
+
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+                    SendQueueItem(channel, properties, body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+
+                    message = "Hello World!!";
+                    body = Encoding.UTF8.GetBytes(message);
+                    SendQueueItem(channel, properties, body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+
+                    message = "Hello World!!!";
+                    body = Encoding.UTF8.GetBytes(message);
+                    SendQueueItem(channel, properties, body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+
+                    message = "Hello World!!!!";
+                    body = Encoding.UTF8.GetBytes(message);
+                    SendQueueItem(channel, properties, body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+        }
+
+        private static void SendQueueItem(IModel channel, IBasicProperties properties, byte[] data)
+        {
+            channel.BasicPublish(exchange: "",
+                                    routingKey: "task_queue",
+                                    basicProperties: properties,
+                                    body: data);
+        }
+    }
+}
